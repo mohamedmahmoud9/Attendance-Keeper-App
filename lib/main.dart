fcfeb90dfabcecb3ebdb872fce2d5567556faba7
ElsayedAbdo219 <habebaelhosary98@gmail.com>
2024-05-20 11:52:44 +0300

    Launching the project

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:attendance_keeper/features/auth/presentation/pages/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'key',
+      appId: 'id',
+      messagingSenderId: 'sendid',
+      projectId: 'myapp',
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
